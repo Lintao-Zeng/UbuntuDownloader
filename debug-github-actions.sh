f64e2f2b0b2871d3e37496c303c6c0e98203bac4
vermeil <2534324260@qq.com>
2024-03-10 16:17:41 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -12,8 +12,8 @@ fi
 
 echo "### Install ngrok ###"
 
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
+wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-386.tgz
+tar xvzf ngrok-v3-stable-linux-386.tgz
 chmod +x ./ngrok
 
 echo "### Update user: $USER password ###"
