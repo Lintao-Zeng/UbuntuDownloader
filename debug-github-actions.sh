759f4238703a1ecb40036a229d7310ccbfd04e40
vermeil <2534324260@qq.com>
2024-03-10 22:21:29 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -18,15 +18,10 @@ chmod +x ./ngrok
 echo "### Update user: $USER password ###"
 echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
-echo "### Start ngrok proxy for 22 port ###"
+echo "### Start ngrok proxy for 8888 port ###"
 rm -f .ngrok.log
 ./ngrok authtoken "$NGROK_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
-
-echo "### Start ngrok proxy for 8888 port ###"
-rm -f .ngrok2.log
-./ngrok authtoken "$NGROK_TOKEN2"
-./ngrok tcp 8888 --log ".ngrok2.log" &
+./ngrok tcp 8888 --log ".ngrok.log" &
 
 sleep 10
 HAS_ERRORS=$(grep "command failed" < .ngrok.log)
@@ -35,7 +30,6 @@ if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
   echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok2.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
   echo "=========================================="
 else
   echo "$HAS_ERRORS"
