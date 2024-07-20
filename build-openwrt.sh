3a198d87abf81226c6ec57d3bf2a815963dfc16e
vermeil <2534324260@qq.com>
2024-07-21 02:01:09 +0800

    Update build-openwrt.sh

--- build-openwrt.sh
+++ build-openwrt.sh
@@ -49,6 +49,6 @@ find dl -size -1024c -exec rm -f {} \;
 # Compile the firmware
 make -j$(nproc) || make -j1 || make -j1 V=s
 
-# firmware path
-cd openwrt/bin/targets/*/*
-pwd
+# Clean packages
+cd openwrt/bin/targets/x86/64
+rm -rf packages
